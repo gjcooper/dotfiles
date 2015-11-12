3565a3b66006ab5133d22b54c351bf74f58cf007
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2015-11-12 21:48:50 +1100

    Load pyflakes (flake8 failing to load)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,9 +13,9 @@ install_sw () {
   # Use apt-get to install any misses pieces of the puzzle
   sudo -E apt-get -y install curl vim gitk zsh tmux python3 ipython ipython3 dos2unix python-pip python3-pip
   #Install flake8 for python 2 and 3
-  sudo -E pip install flake8
+  sudo -E pip install flake8 pyflakes
   sudo mv /usr/local/bin/flake8{,.2}
-  sudo -E pip3 install flake8
+  sudo -E pip3 install flake8 pyflakes
   sudo mv /usr/local/bin/flake8{,.3}
 #  sudo -E pip3 install flake8
 }
