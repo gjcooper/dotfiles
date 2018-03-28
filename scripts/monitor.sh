4493f08bbe0b1a0b86b758a8a895de6772313b10
gjcooper <gjcooper@gmail.com>
2018-03-28 13:49:32 +1100

    Add new option for monitor to switch between (left/right)

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -4,14 +4,14 @@ INTERNAL_OUTPUT="eDP-1"
 
 # if we don't have a file, start at zero
 if [ ! -f "/tmp/monitor_mode.dat" ] ; then
-  monitor_mode="all"
+  monitor_mode="INTERNAL"
 
 # otherwise read the value from the file
 else
   monitor_mode=`cat /tmp/monitor_mode.dat`
 fi
 
-if [ $monitor_mode = "all" ]; then
+if [ $monitor_mode = "RIGHT" ]; then
         monitor_mode="EXTERNAL"
         xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto
 elif [ $monitor_mode = "EXTERNAL" ]; then
@@ -20,8 +20,11 @@ elif [ $monitor_mode = "EXTERNAL" ]; then
 elif [ $monitor_mode = "INTERNAL" ]; then
         monitor_mode="CLONES"
         xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT
-else
-        monitor_mode="all"
+elif [ $monitor_mode = "CLONES" ]; then
+        monitor_mode="LEFT"
         xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --left-of $INTERNAL_OUTPUT
+else
+		monitor_mode="RIGHT"
+		xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT
 fi
 echo "${monitor_mode}" > /tmp/monitor_mode.dat
